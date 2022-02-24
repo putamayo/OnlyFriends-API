d324fe88a7a5560beeb9d9bef2d8f3c7171e18d0
putamayo <cloud@native.shopping>
2022-02-24 14:33:42 +0100

    folder changed

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "co.putamayo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
