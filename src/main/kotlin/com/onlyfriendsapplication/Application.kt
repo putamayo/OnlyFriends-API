837e479782a5b4e8115bc246edefa419d6fc9b07
putamayo <cloud@native.shopping>
2022-02-24 19:13:40 +0100

    webserver created plus conf file

--- src/main/kotlin/com/onlyfriendsapplication/Application.kt
+++ src/main/kotlin/com/onlyfriendsapplication/Application.kt
@@ -1,9 +1,18 @@
 package com.onlyfriendsapplication
 
 import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlin.text.get
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args);
 
-fun Application.modul() {
+fun Application.module() { //Responds to our clients
+    routing{
 
+        get("/") {
+            call.respondText ("Webserver is running. Api response works :)"){ println("test") }
+        }
+
+    }
 }
