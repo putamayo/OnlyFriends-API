600892c824186f4ddf61c55e0ae8bdcacf3de011
putamayo <cloud@native.shopping>
2022-02-25 12:47:15 +0100

    embeddedServer vs Engine main

--- src/main/kotlin/com/onlyfriendsapplication/Application.kt
+++ src/main/kotlin/com/onlyfriendsapplication/Application.kt
@@ -1,18 +1,25 @@
 package com.onlyfriendsapplication
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import kotlin.text.get
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args);
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-fun Application.module() { //Responds to our clients
-    routing{
+@Suppress("unused")
 
+fun Application.module() {
+   //Responds to our clients
+    install(CallLogging)
+    install(Routing) {
         get("/") {
-            call.respondText ("Webserver is running. Api response works :)"){ println("test") }
+            call.respondText("Server works fine!")
         }
-
     }
-}
+    }
+
