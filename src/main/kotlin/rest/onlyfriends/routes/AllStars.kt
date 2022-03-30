2519a4e5d694a2b0da98f723406ce64591a8684c
Rietveldt <74226561+putamayo@users.noreply.github.com>
2022-03-30 22:01:10 +0200

    second endpoint added

--- src/main/kotlin/rest/onlyfriends/routes/AllStars.kt
+++ src/main/kotlin/rest/onlyfriends/routes/AllStars.kt
@@ -1,7 +1,12 @@
 package rest.onlyfriends.routes
 
+import io.ktor.application.*
 import io.ktor.routing.*
+import org.koin.ktor.ext.get
 
 fun Route.getAllStars() {
+    get("/onlyfriends/stars/") {
+        val page = call.request.queryParameters["page"]?.toInt()
+    }
 
 }
