6d5019748d3057627f9dc1798b8f98868f33f6a1
Rietveldt <74226561+putamayo@users.noreply.github.com>
2022-03-31 13:26:30 +0200

    routing pages

--- src/main/kotlin/rest/onlyfriends/plugins/Routing.kt
+++ src/main/kotlin/rest/onlyfriends/plugins/Routing.kt
@@ -5,12 +5,14 @@ import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import rest.onlyfriends.routes.getAllStars
 import rest.onlyfriends.routes.root
 
 fun Application.configureRouting() {
 
     routing {
         root()
+        getAllStars()
     }
 }
 
