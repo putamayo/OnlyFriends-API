a94ce0134069e793900bcfd7ea4b32de9a9e5ef2
Rietveldt <74226561+putamayo@users.noreply.github.com>
2022-04-08 23:20:17 +0200

    testendpoints

--- src/main/kotlin/rest/onlyfriends/plugins/Routing.kt
+++ src/main/kotlin/rest/onlyfriends/plugins/Routing.kt
@@ -6,9 +6,11 @@ import io.ktor.application.*
 import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.request.*
+import org.koin.ktor.ext.get
 import rest.onlyfriends.routes.getAllStars
 import rest.onlyfriends.routes.root
 import rest.onlyfriends.routes.searchStars
+import javax.naming.AuthenticationException
 
 fun Application.configureRouting() {
 
@@ -16,6 +18,7 @@ fun Application.configureRouting() {
         root()
         getAllStars()
         searchStars()
+
 
         static("/images") {
             resources("images")
--- src/main/kotlin/rest/onlyfriends/plugins/Status.kt
+++ src/main/kotlin/rest/onlyfriends/plugins/Status.kt
@@ -4,14 +4,21 @@ import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
+import javax.naming.AuthenticationException
 
 fun Application.configureStatusPage() {
     install(StatusPages) {
-        status(HttpStatusCode.NotFound){
-        call.respond(
-            message = "Helaas pindakaas!",
-            status = HttpStatusCode.NotFound
-        )
+        status(HttpStatusCode.NotFound) {
+            call.respond(
+                message = "Helaas pindakaas!",
+                status = HttpStatusCode.NotFound
+            )
+        }
+        exception<AuthenticationException>{
+            call.respond(
+                message = "Authentication Exception",
+                status = HttpStatusCode.OK
+            )
+        }
     }
-}
 }
