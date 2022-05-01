7b7dd94cc862442ba6f06f68cafb17d0ee7b1056
putamayo <cloud@native.shopping>
2022-05-01 23:30:41 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,25 +5,37 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.10"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    id ("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "rest.onlyfriends"
 version = "0.0.1"
-val koin_version= "3.2.0-beta-1"
+val koin_version = "2.2.3"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 }
+
 
 tasks.create("stage") {
     dependsOn("installDist")
 }
 
+//shadowJar {
+//    archiveFileName = 'app.jar'
+//    manifest {
+//        attributes 'Main-Class': mainClassName
+//    }
+//}
+
 repositories {
     mavenCentral()
 }
 
+
 dependencies {
+    implementation("com.github.jengelman.gradle.plugins:shadow:6.1.0")
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-serialization:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
@@ -32,6 +44,6 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.36")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation ("com.google.firebase:firebase-admin:8.1.0")
+    implementation("com.google.firebase:firebase-admin:8.1.0")
 
 }
